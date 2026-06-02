5faa798cb91ae6407f3af709365dc5c514feddc4
Arthur Faturini <arthurfaturinirpa@gmail.com>
2026-06-02 17:39:40 -0300

    feat: validacao do tamanho de caracteres nos campos

--- back-end/cadastrar.php
+++ back-end/cadastrar.php
@@ -14,49 +14,52 @@
         // Validação se os campos estão vazios
         if (empty(trim($nome)) || empty(trim($email)) || empty($senha) || empty($confirmar_senha)) {
             $erro = "Preencha todos os campos.";
+        // Validação do tamanho de caracteres no campo nome
+        } elseif (strlen($nome) > 255){
+            $erro = "Nome muito grande! Digite um nome com menos caracteres.";
+        // Validação do tamanho da senha
+        } elseif (strlen($senha) < 8){
+            $erro = "Senha muito pequena. Digite pelo menos 8 caracteres";
+        // Validação de confirmação de senha
         } elseif ($senha !== $confirmar_senha) {
             $erro = "As senhas não conferem.";
-        } else {
-            // Validação do email
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $erro = "Erro ao cadastrar: Por favor, insira um email válido. Por exemplo: seu-email@dominio.com";
-            }else{
-                // Verificando se email digitado já existe no BD
-                $stmt_email = mysqli_prepare($conexao, "SELECT EXISTS(SELECT 1 FROM usuarios WHERE email = ?) AS email_existe;"); // Retorna 1 se email existe, e 0 se email NÃO existe
-    
-                mysqli_stmt_bind_param($stmt_email, "s", $email);
-                if(mysqli_stmt_execute($stmt_email)){
-                    $result = mysqli_stmt_get_result($stmt_email);
-                    if($usuario = mysqli_fetch_assoc($result)){
-                        if($usuario['email_existe']){
-                            $erro = "Erro ao cadastrar: Email já cadastrado!";
-                        }
+        // Validação do email
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $erro = "Erro ao cadastrar: Por favor, insira um email válido. Por exemplo: seu-email@dominio.com";
+        // Verificando se email digitado já existe no BD
+        } elseif (!empty($email)){
+            $stmt_email = mysqli_prepare($conexao, "SELECT EXISTS(SELECT 1 FROM usuarios WHERE email = ?) AS email_existe;"); // Retorna 1 se email existe, e 0 se email NÃO existe
+            mysqli_stmt_bind_param($stmt_email, "s", $email);
+            if(mysqli_stmt_execute($stmt_email)){
+                $result = mysqli_stmt_get_result($stmt_email);
+                if($usuario = mysqli_fetch_assoc($result)){
+                    if($usuario["email_existe"]){
+                        $erro = "Erro ao cadastrar: Já existe uma conta associada a este endereço de e-mail.";
                     }
-                }else{
-                    $erro = "Erro ao cadastrar: " . mysqli_stmt_error($stmt_email);
                 }
-    
-                mysqli_stmt_close($stmt_email);
+            }else{
+                $erro = "Erro ao cadastrar: " . mysqli_stmt_error($stmt_email);
             }
-            
-            // Se o email digitado não existir no BD ou nenhum outro erro acontecer, o cadastro pode prosseguir
-            if(empty($erro)){
-                // NUNCA armazene a senha em texto plano — use password_hash()
-                $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
-    
-                $stmt = mysqli_prepare($conexao, "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
-                mysqli_stmt_bind_param($stmt, "sss", $nome, $email, $senha_hash);
-    
-                if (mysqli_stmt_execute($stmt)) {
-                    mysqli_stmt_close($stmt);
-                    mysqli_close($conexao);
-                    header("Location: login.php");
-                    exit;
-                } else {
-                    $erro = "Erro ao cadastrar: " . mysqli_stmt_error($stmt);
-                }
+
+            mysqli_stmt_close($stmt_email);
+        }
+        //Se não tiver nenhum erro, executa o código de cadastro
+        if(empty($erro)){
+            // NUNCA armazene a senha em texto plano — use password_hash()
+            $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+
+            $stmt = mysqli_prepare($conexao, "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
+            mysqli_stmt_bind_param($stmt, "sss", $nome, $email, $senha_hash);
+
+            if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_close($stmt);
+                mysqli_close($conexao);
+                header("Location: login.php");
+                exit;
+            } else {
+                $erro = "Erro ao cadastrar: " . mysqli_stmt_error($stmt);
             }
+            mysqli_stmt_close($stmt);
         }
     }
 
