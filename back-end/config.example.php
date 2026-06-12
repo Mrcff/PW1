82c4103212dddba481d1ad1730d52618c2efb8cf
Jamily <milyprotschnot@gmail.com>
2026-06-11 21:56:53 -0300

    feat: tabelas no banco implementadas (liga e partida)

--- back-end/config.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-                           // Padrão:
-    define('DB_HOST', ''); // localhost
-    define('DB_PORT', ''); // 3306
-    define('DB_USER', ''); // root
-    define('DB_PASS', ''); // 
-    define('DB_NAME', ''); // web1
-?>  
--- back-end/tables.php
+++ back-end/tables.php
@@ -17,5 +17,45 @@
         echo "Tabela 'usuarios' pronta!";
     }
 
+    $tabelas = [
+        //tabela que armazena a pontuação de cada jogador em uma tentativa de jogo
+       "partida" => "CREATE TABLE IF NOT EXISTS partida(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            usuario_id INT NOT NULL,
+            pontuacao INT NOT NULL,
+            dataPartida TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE RESTRICT
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4",
+
+        "liga" => "CREATE TABLE IF NOT EXISTS liga(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(255) NOT NULL UNIQUE,
+            palavraChave VARCHAR(255) NOT NULL UNIQUE,
+            criador_id INT,
+            dataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+            dataFim DATE,
+            FOREIGN KEY (criador_id) REFERENCES usuarios(id) ON DELETE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4",
+
+        "ligaUsuario" => "CREATE TABLE IF NOT EXISTS ligaUsuario (
+            liga_id INT,
+            usuario_id INT,
+            dataEntrada TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(liga_id, usuario_id),
+            FOREIGN KEY (liga_id) REFERENCES liga(id) ON DELETE CASCADE,
+            FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4"
+    ];
+
+    foreach ($tabelas as $nome => $sql) {
+        mysqli_query($conexao, $sql);
+        if (mysqli_error($conexao)) {
+            echo "Erro ao criar tabela $nome: " . mysqli_error($conexao) . "<br>";
+        } else {
+            echo "Tabela '$nome' pronta!<br>";
+        }
+    }
+
     mysqli_close($conexao);
+
 ?>
