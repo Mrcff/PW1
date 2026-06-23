a145d1fa37b5a1d5d3b6d6b919d64fa2a2c5b7bf
Jamily <milyprotschnot@gmail.com>
2026-06-23 01:07:50 -0300

    feat: cenario, personagens, estilização

--- front-end/components/menu.php
+++ front-end/components/menu.php
@@ -3,8 +3,8 @@ $base = "/trabalhoWeb/PW1/front-end/";
 ?>
 <header class="site-header">
 
-    <a href="index.php" class="header-logo">
-        Café do TADS
+    <a href="<?= $base ?>index.php" class="header-logo">
+        Café sem Fronteiras
     </a>
 
     <nav class="header-nav">
@@ -17,9 +17,9 @@ $base = "/trabalhoWeb/PW1/front-end/";
     <div class="header-right">
         <?php if (isset($_SESSION["usuario_id"])): ?>
             <span>Olá, <?= htmlspecialchars($_SESSION["usuario_nome"]) ?></span>
-            <a href="../../back-end/login/logout.php">Sair</a>
+            <a href="/trabalhoWeb/PW1/back-end/login/logout.php">Sair</a>
         <?php else: ?>
-            <a href="../back-end/login/login.php">Login</a>
+            <a href="/trabalhoWeb/PW1/back-end/login/login.php">Login</a>
         <?php endif; ?>
     </div>
 
---
+++ front-end/data/cenarios.js
@@ -0,0 +1,38 @@
+/*
+ * Documentacao:
+ * - Um nivel sorteia uma entrada deste catalogo.
+ * - Reiniciar a fase preserva o cenario atual.
+ * - Para adicionar um pais, inclua fundo, mesa e arvore transparentes
+ *   nas pastas de assets e crie uma entrada com um id unico.
+ */
+window.CenariosJogo = [
+  {
+    id: "brasil",
+    pais: "Brasil",
+    fundo: "../assets/images/fundo/brasil.png",
+    mesa: "../assets/images/mesa/brasil.webp",
+    arvore: "../assets/images/arvore/brasil.webp",
+  },
+  {
+    id: "franca",
+    pais: "França",
+    fundo: "../assets/images/fundo/franca.png",
+    mesa: "../assets/images/mesa/franca.webp",
+    arvore: "../assets/images/arvore/franca.png",
+  },
+  {
+    id: "japao",
+    pais: "Japão",
+    fundo: "../assets/images/fundo/japao.png",
+    mesa: "../assets/images/mesa/japao.webp",
+    arvore: "../assets/images/arvore/japao.png",
+  },
+  {
+    id: "newyork",
+    pais: "Estados Unidos",
+    cidade: "Nova York",
+    fundo: "../assets/images/fundo/newyork.png",
+    mesa: "../assets/images/mesa/newyork.webp",
+    arvore: "../assets/images/arvore/newyork.png",
+  },
+];
